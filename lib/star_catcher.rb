e85c2982af5a0eb09e9fee2410ae7547b56d6158
freqn <freqn@freqns-MacBook-Pro-2.local>
2018-12-25 15:04:57 -0500

    slight refactor

--- lib/star_catcher.rb
+++ lib/star_catcher.rb
@@ -16,19 +16,23 @@ class StarCatcher
 
   def export
     File.open("../data/data.txt","w") do |f|
-      get_starred.each do |x|
-        if x["message"]["attachments"]
-          item = {
-            id: @id += 1,
-            title:  x["message"]["attachments"][0]["title"] || "",
-            desc:   x["message"]["attachments"][0]["text"] || "",
-            link:   x["message"]["attachments"][0]["title_link"] || ""
-          }
-          f.write("#{item}\n")
-        end
-      end
+      build_file(f)
     end
     log.info("DONE! Your file is now ready in the data directory!")
+  end
+
+  def build_file(file)
+    get_starred.each do |x|
+      if x["message"]["attachments"]
+        item = {
+          id: @id += 1,
+          title:  x["message"]["attachments"][0]["title"] || "",
+          desc:   x["message"]["attachments"][0]["text"] || "",
+          link:   x["message"]["attachments"][0]["title_link"] || ""
+        }
+        file.write("#{item}\n")
+      end
+    end
   end
 end
 
