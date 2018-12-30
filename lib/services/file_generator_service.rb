4cde49e60b2b25829564a25aab9c7629ae72e582
freqn <freqn@freqns-MacBook-Pro-2.local>
2018-12-30 11:39:05 -0500

    refactor & separate responsibilities

---
+++ lib/services/file_generator_service.rb
@@ -0,0 +1,62 @@
+require_relative './client_service.rb'
+require_relative './log_service.rb'
+
+module Service
+  class FileGeneratorService
+    attr_reader :client, :log
+
+    def initialize
+      @log = LogService.new
+      @client = ClientService.new
+    end
+
+    def get_starred
+      client.stars_list
+    end
+
+    def generate
+      File.open("../data/data.txt","w") do |f|
+        generate_file(f)
+      end
+      log_success
+    end
+    
+    private
+  
+    def generate_file(file)
+      @id = 0
+      get_starred.each do |x|
+        if data_valid?(x)
+          item =    build_data(x)
+          record =  formatted_data(item)
+          file.write("#{record}\n")
+        end
+      end
+    end
+
+    def data_valid?(x)
+      x["message"]["attachments"] ? true : false
+    end
+  
+    Data = Struct.new(:id, :title, :desc, :link)
+  
+    def build_data(item)
+      Data.new(@id += 1,
+              item["message"]["attachments"][0]["title"] || "",
+              item["message"]["attachments"][0]["text"] || "",
+              item["message"]["attachments"][0]["title_link"] || "")
+    end
+  
+    def formatted_data(item)
+      { id:     item.id,
+        title:  item.title,
+        desc:   item.desc,
+        link:   item.link
+      }
+    end
+    
+    def log_success
+      log.info("DONE! Your file is now ready in the data directory!")
+    end
+  end
+end
--- lib/star_catcher.rb
+++ lib/star_catcher.rb
@@ -1,42 +1,11 @@
-require_relative './services/client_service.rb'
-require_relative './services/log_service.rb'
+require_relative './services/file_generator_service.rb'
 
 class StarCatcher
   include Service
-  attr_reader :client, :log
   
   def initialize
-    @log    = Service::LogService.new
-    @client = Service::ClientService.new
-    @id     = 0
-  end
-
-  def get_starred
-    client.stars_list
-  end
-
-  def export
-    File.open("../data/data.txt","w") do |f|
-      build_file(f)
-    end
-    log.info("DONE! Your file is now ready in the data directory!")
-  end
-
-  private
-
-  def build_file(file)
-    get_starred.each do |x|
-      if x["message"]["attachments"]
-        item = {
-          id: @id += 1,
-          title:  x["message"]["attachments"][0]["title"] || "",
-          desc:   x["message"]["attachments"][0]["text"] || "",
-          link:   x["message"]["attachments"][0]["title_link"] || ""
-        }
-        file.write("#{item}\n")
-      end
-    end
+    FileGeneratorService.new.generate
   end
 end
 
-StarCatcher.new.export
+StarCatcher.new
