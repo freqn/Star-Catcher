2c4c78ad7c19bc9761bfadac2074f35ce8817266
freqn <ceyler@gmail.com>
2019-09-25 14:09:40 -0400

    remove whitespace

--- lib/services/file_generator_service.rb
+++ lib/services/file_generator_service.rb
@@ -10,20 +10,19 @@ module Service
       @log    = LogService.new
     end
 
-    
     def generate
       File.open("../data.txt","w") do |f|
         generate_file(f)
       end
       log_success
     end
-    
+
     private
-    
+
     def stars
       client.stars_list["items"]
     end
-    
+
     def generate_file(file)
       @id = 0
       stars.each do |x|
@@ -34,7 +33,7 @@ module Service
         end
       end
     end
-    
+
     def append(file, record)
       file.write("#{record}\n")
     end
@@ -42,16 +41,16 @@ module Service
     def data_valid?(x)
       x["message"]["attachments"] ? true : false
     end
-  
+
     Data = Struct.new(:id, :title, :desc, :link)
-  
+
     def structured_data(item)
       Data.new(@id += 1,
               item["message"]["attachments"][0]["title"] || "",
               item["message"]["attachments"][0]["text"] || "",
               item["message"]["attachments"][0]["title_link"] || "")
     end
-  
+
     def formatted_data(item)
       { id:     item.id,
         title:  item.title,
@@ -59,7 +58,7 @@ module Service
         link:   item.link
       }
     end
-    
+
     def log_success
       log.info("DONE! Your file is now ready in data.txt!")
     end
